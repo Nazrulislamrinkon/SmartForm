b621d907637c5af5854aafc42ce35c41eb9b9954
Nazrulislamrinkon <nazrulislamrinkon@gmail.com>
2018-02-12 09:29:14 +0600

    Add files via upload

--- smartForm.js
+++ smartForm.js
@@ -1,3 +1,33 @@
 // Your jQuery goes here
 
-var userData = {};
+var userData = {
+
+	name : '',
+	email: '',
+	html:{likes:[],dislikes:[]},
+	css:{likes:[],dislikes:[]},
+	js:{likes:[],dislikes:[]},
+	strength:{css: '',js: '',html:''},
+	currentQuestion: '#welcome'
+};
+	$('#start').click(function){
+	console.log("start");
+	$('#welcome').hide();
+	$('#q1').show();
+});
+
+$('#name').change(function(event){
+	//console.log(event);
+	console.log($('#name').val());
+});
+$('#q1next').click(function(){
+
+	userData.name = $('#name').val() && $('#email').val()){
+	userData.name = $('#email').val();
+	$('#q2').show();
+	$(#q1).hide();
+}
+else{
+	alert('please enter name & email');
+}
+});
